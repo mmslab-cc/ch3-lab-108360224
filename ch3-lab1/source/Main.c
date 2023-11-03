3b5f1524c6aaa5b5f1f3bf51ed322017a62a22a8
108360224 <>
2023-11-03 16:27:14 +0800

    first

--- ch3-lab1/source/Main.c
+++ ch3-lab1/source/Main.c
@@ -6,11 +6,47 @@
 
 int main(void)
 {
+	int n, sum; 
+	char choice;
 	printf("1+2+...+n=?請輸入n=\n");
 	printf("請問要做奇數和(O),偶數和(E),還是整數和(I)?請選擇:\n");
-	// 在這裡撰寫程式
-	system("pause");
+	scanf(" %c", &choice);
+	switch (choice) {
+	case 'O': sum = Odd(n); 
+		break; 
+	case 'E': sum = Even(n); 
+		break; 
+	case 'I': sum = TotalSum(n); 
+		break; 
+	default: 
+		printf("選擇錯誤\n"); 
+		return -1;
+	} 
+	printf("總合為%d\n", sum); 
+	system("pause"); 
 	return 0;
+
+
 }
 
-// 在這裡定義函式
+int Odd(int n) {
+	int i, total = 0; 
+	for (i = 1; i <= n; i++)
+		if (i % 2 == 1)
+			total = total + i;
+	return total;
+}
+int Even(int n) {
+	int i, total = 0; 
+	for (i = 1; i <= n; i++) 
+		if (i % 2 == 0) 
+			total = total + 1; 
+	return total;
+	
+
+}
+int TotalSum(int n) {
+	return Odd(n) + Even(n);
+}
+
+
